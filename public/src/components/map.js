84806a317dd38bc28708119f5327b5f87bdd3c3b
sharonchen9593 <sharonchen9593@gmail.com>
2017-07-07 17:45:59 -0700

    lat, lng and zoom changes with state

--- public/src/components/map.js
+++ public/src/components/map.js
@@ -2,18 +2,24 @@ import React from 'react';
 import {withGoogleMap, GoogleMap, Marker} from 'react-google-maps';
 
 class Map extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state={
+			lat: 37.0902,
+			lng: -95.7129,
+			zoom: 4
+		}
+	}
 	render() {
 		var markers = this.props.markers || []
 		return (
 				<GoogleMap
-					defaultZoom={4}
-				  defaultCenter={{lat:37.0902 , lng:-95.7129}}>
+					defaultZoom={this.state.zoom}
+				  defaultCenter={{lat:this.state.lat, lng:this.state.lng}}>
 				  {markers.map((marker, index)=>(
 				  		<Marker {...marker} />
 				  	)
 				  )}
-
-
 				</GoogleMap>
 		)
 	}
