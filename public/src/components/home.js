8a9bd1ffbf49dc7482000d316e85ef1891aab658
sharonchen9593 <sharonchen9593@gmail.com>
2017-07-09 16:31:24 -0700

    faded images on bottom of home page

--- public/src/components/home.js
+++ public/src/components/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import HomeMap from './homemap'
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -6,7 +7,56 @@ export default class Home extends React.Component {
   }
   render() {
     return (
-      <div className="page"> Home </div>
+      <div className="page">
+        <div className="homebg">
+        RANDOM PICTURE
+        <img src="https://s3-media3.fl.yelpcdn.com/bphoto/nNwtDkoEd7GN5m3f2eCpZg/348s.jpg"/>
+        </div>
+        <div className="homemap">
+          <HomeMap
+            containerElement={<div style={{height: '100%'}} />}
+            mapElement={<div style={{height: '100%'}} />}/>
+        </div>
+
+
+        <div className="banner" style={{backgroundImage: "url('../../images/dog.jpeg')", height: "200px", width: "200px"}}>
+        NUMBER
+        <br/>
+        Dogs need a home
+        </div>
+
+        <div className="banner" style={{backgroundImage: "url('../../images/cat.jpeg')", height: "200px", width: "200px"}}>
+        NUMBER
+        <br/>
+        Cats need a home
+        </div>
+
+        <div className="banner" style={{backgroundImage: "url('../../images/bird.jpeg')", height: "200px", width: "200px"}}>
+        NUMBER
+        <br/>
+        Birds need a home
+        </div>
+
+        <div className="banner" style={{backgroundImage: "url('../../images/reptile.jpeg')", height: "200px", width: "200px"}}>
+        NUMBER
+        <br/>
+        Reptiles need a home
+        </div>
+
+        <div className="banner" style={{backgroundImage: "url('../../images/smallfurry.jpeg')", height: "200px", width: "200px"}}>
+        NUMBER
+        <br/>
+        Small animals need a home
+        </div>
+
+        <div className="banner" style={{backgroundImage: "url('../../images/horse.jpeg')", height: "200px", width: "200px"}}>
+        NUMBER
+        <br/>
+        Horses need a home
+        </div>
+
+      </div>
     )
   }
 }
+          // <img src = "../../images/dog.jpeg" className="bannerimg" />
---
+++ public/src/components/homemap.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import {withGoogleMap, GoogleMap, Marker} from 'react-google-maps';
+import axios from 'axios';
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      lat: 37.0902,
+      lng: -95.7129,
+      zoom: 4
+    }
+  }
+
+  changeState(lat, lng, zoom) {
+
+    this.setState({lat: lat, lng: lng, zoom: zoom})
+    console.log(this.state.lat, this.state.lng, this.state.zoom)
+  }
+
+  componentDidMount() {
+    var self = this;
+    $.ajax({
+      url: "https://geoip-db.com/jsonp",
+      jsonpCallback: "callback",
+      dataType: "jsonp",
+      success: function( location ) {
+        console.log(location)
+        self.changeState(location.latitude, location.longitude, 10)
+      }
+    });
+  }
+
+  render() {
+    var markers = this.props.markers || []
+    return (
+        <GoogleMap
+          defaultZoom={this.state.zoom}
+          defaultCenter={{lat:this.state.lat, lng:this.state.lng}}>
+          {markers.map((marker, index)=>(
+              <Marker {...marker} />
+            )
+          )}
+        </GoogleMap>
+    )
+  }
+}
+
+export default withGoogleMap(Map)
