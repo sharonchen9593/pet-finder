2da8ac0b54d151725f02a01feab08ca0688c78cc
Anthony Nguyen <anguyee@gmu.edu>
2017-07-13 15:06:41 -0700

    moved over auth from landmarks....signup page not directing correctly

--- public/src/components/signup.js
+++ public/src/components/signup.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import axios from 'axios';
+import {connect} from 'react-redux'
+import {userSignupRequest} from '../../actions';
+import { Redirect } from 'react-router';
 
-export default class SignUp extends React.Component {
+class Signup extends React.Component {
   constructor(props) {
     super(props)
 
     this.state={
-      email: '',
+      username: '',
       password: '',
       confirmPassword: '',
       authenticated: false,
@@ -17,18 +20,42 @@ export default class SignUp extends React.Component {
     this.setState({[e.target.name]: e.target.value})
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+    var username = this.state.username;
+    var password = this.state.password;
+    var confirmPassword = this.state.confirmPassword;
+    var self = this;
+    if (password === confirmPassword) {
+      this.props.userSignupRequest({username, password})     // for redux-thunk
+      // axios.post('/signup', {username, password})
+      // .then(function(response) {
+       //  console.log(response)
+       //  self.setState({authenticated: true, redirect: true})
+       //  localStorage.setItem('token', response.token)
+      // })
+      // .catch(function(error) {
+       //  alert("Invalid Username/Password")
+      // })
+    }
+    else {
+      alert("Password does not match")
+    }
+
+  }
 
   render() {
+    let {isSignupSuccess, signupError} = this.props;
 
     return (
-      <form className = "loginform">
+      <form onSubmit = {this.onSubmit.bind(this)} className = "signinform">
         <h1>Sign Up</h1>
         <br />
-        <label>Email:</label>
+        <label>Username:</label>
         <br />
         <input type="text"
-               name="email"
-               value={this.state.email}
+               name="username"
+               value={this.state.username}
                onChange = {this.onChange.bind(this)}
         />
         <br />
@@ -51,10 +78,25 @@ export default class SignUp extends React.Component {
         <br />
         <button type="submit">Submit</button>
 
+        {isSignupSuccess && <Redirect to= '/profile'/>}
+        {signupError && <div>Invalid Username or Password</div>}
       </form>
     );
   }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    isSignupSuccess: state.isSignupSuccess,
+    signupError: state.signupError
+  }
+}
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    userSignupRequest: (username, password) => dispatch(userSignupRequest(username, password))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Signup)     // for redux-thunk
 
