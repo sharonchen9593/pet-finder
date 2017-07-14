8b8e8eff19257c858ac7712062530168cab1582c
Anthony Nguyen <anguyee@gmu.edu>
2017-07-13 22:01:44 -0700

    login is now working

--- public/src/components/login.js
+++ public/src/components/login.js
@@ -1,12 +1,19 @@
-import React from 'react';
 
-export default class LogIn extends React.Component {
+import axios from 'axios';
+import React from 'react';
+import { Redirect } from 'react-router';
+import {connect} from 'react-redux';
+import {userSigninRequest} from '../../actions';
+import {Nav,Navbar,NavDropdown, MenuItem, NavItem} from 'react-bootstrap'
+
+
+class LogIn extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       authenticated: false,
-      email: '',
+      username: '',
       password: ''
     }
   }
@@ -15,17 +22,36 @@ export default class LogIn extends React.Component {
     this.setState({[e.target.name]: e.target.value})
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+    var username = this.state.username;
+    var password = this.state.password;
+    console.log(username, password);
+    var self = this;
+    this.props.userSigninRequest(this.state);    // for redux thunk
+    // axios.post('/signin', {username, password})
+    // .then(function(response) {
+    //  console.log(response)
+    //  self.setState({authenticated: true, redirect: true})
+    //  localStorage.setItem('token', response.token)
+    // })
+    // .catch(function(error) {
+    //  alert("Invalid Username/Password")
+    // })
+  }
+
   render() {
+    let {isSigninSuccess, signinError} = this.props;
 
     return (
-      <form className="loginform">
+      <form onSubmit = {this.onSubmit.bind(this)} className="signinform">
         <h1>Log In</h1>
         <br />
-        <label>Email:</label>
+        <label>Username:</label>
         <br />
-        <input type="email"
-               name="email"
-               value={this.state.email}
+        <input type="text"
+               name="username"
+               value={this.state.username}
                onChange = {this.onChange.bind(this)}
         />
         <br />
@@ -40,8 +66,25 @@ export default class LogIn extends React.Component {
         <br />
         <button type="submit">Log In</button>
 
+        {isSigninSuccess && <Redirect to= '/profile'/>}
+        {signinError && <div>Invalid Username or Password</div>}
       </form>
 
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    isSigninSuccess: state.isSigninSuccess,
+    signinError: state.signinError
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    userSigninRequest: (username, password) => dispatch(userSigninRequest(username, password))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LogIn)   // for redux-thunk
