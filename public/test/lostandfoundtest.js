818d5c5021e74356de58e3f674bd0e34447f2fb7
sharonchen9593 <sharonchen9593@gmail.com>
2017-07-18 14:13:00 -0700

    tests component renders and button works

--- public/test/lostandfoundtest.js
+++ public/test/lostandfoundtest.js
@@ -1,9 +1,23 @@
 
-
+import 'jsdom-global/register'
+import React from 'react';
 var expect = require('chai').expect
+var assert = require('assert')
+var mount = require('enzyme').mount
+var shallow = require('enzyme').shallow
+import LostAndFound from '../src/components/lostandfound'
 
-describe('Our first test', () => {
-  it('should pass', () => {
-    expect(true).to.equal(true);
+describe("Lost and Found", function() {
+  it('should create object', () => {
+    let wrapper = mount(<LostAndFound />)
+    expect(LostAndFound.prototype).to.not.be.null;
   })
+
+  it('links to new entry page', () => {
+    let wrapper = shallow(<LostAndFound/>);
+    expect(
+      wrapper.find('a').prop('href')
+      ).equal('/newentry')
+  })
+
 })
